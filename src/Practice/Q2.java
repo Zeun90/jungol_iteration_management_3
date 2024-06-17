f304025c82a7e1fc129248b67202ac6f43d09ed3
Lee JiEun <gsilver190@gmail.com>
2024-06-17 09:16:49 +0900

    연습 문제 2

---
+++ src/Practice/Q2.java
@@ -0,0 +1,12 @@
+package Practice;
+
+public class Q2 {
+    public static void main(String[] args) {
+        String st = "*";
+
+        for (int i = 1; i < 4; i++) {
+            System.out.println(st);
+            st += "*";
+        }
+    }
+}
