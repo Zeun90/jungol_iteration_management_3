05475d4ef84ae60d641fb145b6764a8a94d8bf63
Lee JiEun <gsilver190@gmail.com>
2024-06-18 17:24:55 +0900

    형성 평가 3

--- src/Formative/Q3.java
+++ src/Formative/Q3.java
@@ -4,19 +4,18 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Q2 {
+public class Q3 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int input = Integer.parseInt(br.readLine());
-        int result = 0;
+        String star = "*";
 
-        if (1 <= input && input <= 100) {
-            for (int i = 1; result < 100; i++) {
-                result = input * i;
-                System.out.printf("%d ", result);
-                if (result % 10 == 0)
-                    break;
-            }
+        for (int i = 1; i <= input; i++) {
+            System.out.println(star.repeat(i));
+        }
+
+        for (int j = input - 1; j > 0; j--) {
+            System.out.println(star.repeat(j));
         }
     }
 }
